9c87bb631efdab33a155f29e5b9f4829bf8bf1fa
Uomar7 <uomarearlie7@gmail.com>
2019-03-24 15:18:07 +0300

    adjustments made to the response.

--- main.js
+++ main.js
@@ -21,18 +21,18 @@ var lengthab = a + b;
 var respond;
 
 if(lengthab < c){
-  	respond = "ERROR! Not a triangle"
+  	respond = "ERROR! Your values can not  make a triangle"
   }
 else if(a === b && b === c && a > 0 && b > 0 && c > 0){
-  console.log('Equilateral Triangle')
-  respond = 'Equilateral Triangle'
+  console.log('Your dimensions produce an Equilateral Triangle')
+  respond = 'Your dimensions produce an Equilateral Triangle'
 }else if(a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0){
-  console.log('Isosceles Triangle')
-  respond = 'Isoceles Triangle'
+  console.log('Your dimensions produce an Isosceles Triangle')
+  respond = 'Your dimensions produce an Isoceles Triangle'
 }else if(a !== b && b!== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(c) === true) && a > 0 && b > 0 && c > 0){
-  console.log('Scalene Triangle')
-  respond = 'Scalene Triangle'
-}else { respond = "Invalid measurements. Kindly re-evaluate your figures."};
+  console.log('Your dimensions produce a Scalene Triangle')
+  respond = 'Your dimensions produce a Scalene Triangle'
+}else { respond = "Invalid measurements. Kindly use numbers and not words. Have fun!!"};
 
 
 document.querySelector("#result").innerHTML = respond
