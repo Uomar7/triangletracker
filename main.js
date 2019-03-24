1a8abe3b8903406b09088350d0acc9afbde62b0a
Uomar7 <uomarearlie7@gmail.com>
2019-03-24 11:41:46 +0300

    The variables for length have been put in place. Hence the user can now input data.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function calc() {
 
+  var length1 = [parseFloat(document.querySelector("#sideA").value)]
+  var length2 = [parseFloat(document.querySelector("#sideB").value)]
+  var length3 = [parseFloat(document.querySelector("#sideC").value)]
 
 
 
